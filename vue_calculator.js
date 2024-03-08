f05ff794d1c299133017cc7aad255a3d79f16538
manejtan <110987852+manejtan@users.noreply.github.com>
2024-03-07 23:41:00 -0500

    Update vue_calculator.js

--- vue_calculator.js
+++ vue_calculator.js
@@ -7,27 +7,27 @@ const app = createApp({
     data() {
         return {
             input: "",
-            error: false,
-            beforePercent: "",
-            afterSqrt: "",
-            foundParen : false,
+            undo: [],
         };
-    },
-    computed : {
-        noSpaces: function(){
-            return this.input.replace(/ /g, '');
-        }
-    },
-    watch : {
-        noNewLines() {
-            this.input = this.input.replace(/\n/g, '');
-        }
     },
     methods: {
         add(value) {
             this.input += value;
         },
-        updateInput(event){
+        remove() {
+            if(this.input.length != 0){
+                this.undo.push(this.input);
+                this.input = "";
+                console.log("yes");
+            }
+        },
+        undothis() {
+            if (this.undo.length != 0) {
+                this.input = "";
+                this.input += this.undo.pop();
+            }
+        },
+        updateInput(event) {
             this.input = event.target.innerHTML;
         },
         calculate() {
@@ -39,29 +39,29 @@ const app = createApp({
                         for (let j = 0; j < this.beforePercent.length; j++) {
                             if (this.beforePercent[j] == "(" && this.foundParen == false) {
                                 this.foundParen = true;
-                                this.input = this.input.replace("%","")
-                                this.input = this.input.replace(this.beforePercent.substring(j+1,i-1), eval(this.beforePercent.substring(j+1,i-1)) * 0.01)
+                                this.input = this.input.replace("%", "")
+                                this.input = this.input.replace(this.beforePercent.substring(j + 1, i - 1), eval(this.beforePercent.substring(j + 1, i - 1)) * 0.01)
                             }
                         }
                     }
                     else {
-                        this.input = this.input.slice(0, i - 1) + this.input[i-1]*0.01 + this.input.slice(i + 1)
+                        this.input = this.input.slice(0, i - 1) + this.input[i - 1] * 0.01 + this.input.slice(i + 1)
                     }
                 }
                 if (this.input[i] == '√') {
                     if (this.input[i + 1] == '(') {
                         this.foundParen = false;
-                        this.afterSqrt = this.input.slice(i+1);
+                        this.afterSqrt = this.input.slice(i + 1);
                         for (let j = 0; j < this.afterSqrt.length; j++) {
                             if (this.afterSqrt[j] == ")" && this.foundParen == false) {
                                 this.foundParen = true;
-                                this.input = this.input.replace("√","")
-                                this.input = this.input.replace(this.afterSqrt.substring(i,j+1), Math.sqrt(eval(this.afterSqrt.substring(i+1,j))))
+                                this.input = this.input.replace("√", "")
+                                this.input = this.input.replace(this.afterSqrt.substring(i, j + 1), Math.sqrt(eval(this.afterSqrt.substring(i + 1, j))))
                             }
                         }
                     }
                     else {
-                        this.input = this.input.slice(0, i) + Math.sqrt(this.input[i+1]) + this.input.slice(i + 2)
+                        this.input = this.input.slice(0, i) + Math.sqrt(this.input[i + 1]) + this.input.slice(i + 2)
                     }
                 }
                 if (this.input[i] == 'm' && this.input[i + 1] == 'o' && this.input[i + 2] == 'd') {
@@ -71,30 +71,30 @@ const app = createApp({
                     this.input = this.input.slice(0, i) + "**" + this.input.slice(i + 1)
                 }
                 if (this.input[i] == 'π') {
-                    if (this.input[i + 1] == 'π') {
-                        this.input = this.input.slice(0, i) + "Math.PI * "+ this.input.slice(i + 1)
-                    } else{
                     this.input = this.input.slice(0, i) + "Math.PI" + this.input.slice(i + 1)
-                    }
                 }
                 if (this.input[i] == '(') {
                     if (this.input.indexOf(')') === -1) {
                         this.input = this.input.replace("(", "");
                     }
                 }
-                if (this.input[i] == ')') {
-                    if (this.input[i + 1] == '(') {
-                        this.input = this.input.slice(0, i) + ")*" + this.input.slice(i + 1)
-                    }
+            }
+            console.log(this.input)
+            try { 
+                if(this.input.length != 0){
+                    this.undo.push(this.input);
                 }
-                }
-                console.log(this.input);
-            try { this.input = eval(this.noSpaces);} catch(error) {this.input = "Error";}
+                this.input = (eval(this.input)).toString(); 
+            } 
+            catch (error) { 
+                this.input = "Error" 
+                console.log(error);
+            }
         },
         handleKeyPress(event) {
             if (event.key === "Enter") {
-              event.preventDefault();
-              this.calculate();
+                event.preventDefault();
+                this.calculate();
             }
         },
     },
